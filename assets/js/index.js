e05e4c52ede7ff10e9fb382b9618526e06cf9faf
Romina Osorio <r.osorio.marmolejo@gmail.com>
2025-08-22 10:39:20 -0400

    Se umplmente alerta para envio de formulario

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,15 +2,34 @@
   'use strict'
 
   const forms = document.querySelectorAll('.needs-validation')
+  const formAlert = document.getElementById('formAlert')
+
   Array.from(forms).forEach(function (form) {
     form.addEventListener(
       'submit',
       function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+        event.preventDefault()
+        event.stopPropagation()
+
+        if (form.checkValidity()) {
+          formAlert.innerHTML = `
+          <div class="alert alert-success alert-dismissible fade show" role="alert">
+            ¡Agendamiento realizado exitosamente!
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>
+        `
+          form.reset()
+          form.classList.remove('was-validated')
+
+          const fechaInput = document.getElementById('fechaInput')
+          const hoy = new Date()
+          const dia = hoy.getDate().toString().padStart(2, '0')
+          const mes = (hoy.getMonth() + 1).toString().padStart(2, '0')
+          const anio = hoy.getFullYear()
+          fechaInput.value = `${anio}-${mes}-${dia}`
+        } else {
+          form.classList.add('was-validated')
         }
-        form.classList.add('was-validated')
       },
       false
     )
